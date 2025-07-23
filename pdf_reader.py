a9d85cc023b5cc9d69201a8602614082cdebb9ba
Muhammad Haider Ijaz <haiderijaz@users.noreply.huggingface.co>
2025-07-23 17:42:47 +0000

    Update pdf_reader.py

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,8 +1,11 @@
 import pdfplumber
+import io
 
-def extract_text_from_pdf(file):
+def extract_text_from_pdf(file_bytes):
     text = ""
-    with pdfplumber.open(file) as pdf:
+    with pdfplumber.open(io.BytesIO(file_bytes)) as pdf:
         for page in pdf.pages:
-            text += page.extract_text() + "\n"
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text + "\n"
     return text
